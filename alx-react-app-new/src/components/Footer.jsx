c59106b482731a8ef36494b1d9b01186f21fb7ea
Omar Albaz <omaralbaz635@gmail.com>
2024-11-10 01:27:16 +0200

    creat : alx-react-app-new

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return(
-        <footer>
+        <footer style={{color:'red'}}>
              <p>© 2023 City Lovers</p>
         </footer>
     )
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main>
+        <main style={{color:'red'}}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     )
