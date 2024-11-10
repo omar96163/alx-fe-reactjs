9e9829dfcf9e9d19b7a7ff8d148fb8b48a520376
Omar Albaz <omaralbaz635@gmail.com>
2024-11-10 06:42:32 +0200

    creat : alx-react-app-props

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,15 +1,15 @@
 import ProfilePage from './components/ProfilePage';
-import { UserContext } from './components/UserContext';
+import UserContext from './components/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return ( 
-  <UserContext.Provider value={userData}>
-    <ProfilePage userData={userData} />
-  </UserContext.Provider>
-  
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+    
   )
-  }
+}
 
 export default App;
--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
-import { UserContext } from "./UserContext";
+import UserContext from "./UserContext";
+
 
 function UserDetails() {
     const userData = useContext(UserContext)
