7d9493473592e009ecc49e3dade22feadac5978a
Omar Albaz <omaralbaz635@gmail.com>
2024-11-10 03:42:00 +0200

    creat : alx-react-app-props

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { UserContext } from "./UserContext";
 
-function UserDetails({ userData }) {
+function UserDetails() {
     const useData =  useContext(UserContext)
     return (
       <div>
