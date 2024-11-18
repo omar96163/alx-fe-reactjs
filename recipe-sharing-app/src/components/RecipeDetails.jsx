4dac96db01837dd6d7e38f0f92153eef4d3a61c3
Omar Albaz <omaralbaz635@gmail.com>
2024-11-18 06:56:38 +0200

    creat :  recipe-sharing-app

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,11 +1,12 @@
-// src/components/RecipeDetails.jsx
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useRecipeStore from './recipeStore';
 
 const RecipeDetails = () => {
   const { id } = useParams();
-  const recipe = useRecipeStore((state) => state.recipes.find((r) => r.id === Number(id)));
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((r) => r.id === id || r.id === Number(id))
+  );
 
   if (!recipe) return <p>Recipe not found.</p>;
 
