7bfd49623f6cf4c9dd42921cdd1b0dc5abbf5690
Omar Albaz <omaralbaz635@gmail.com>
2024-11-24 16:24:58 +0200

    creat : react todo

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,40 @@
+import { screen , fireEvent , render } from "@testing-library/react";
+import TodoList from "./TodoList";
+import '@testing-library/jest-dom'
+
+
+test('all todo', () => { 
+    render(<TodoList />)
+    expect(screen.getByText('item 1')).toBeInTheDocument()
+    expect(screen.getByText('item 2')).toBeInTheDocument()
+})
+
+
+test('add todo', () => { 
+    render(<TodoList />);
+    const input = screen.getByPlaceholderText('add todo');
+    const button = screen.getByText('add');
+
+    fireEvent.change(input, { target: { value: 'new todo' } });
+    fireEvent.click(button);
+
+    expect(screen.getByText('new todo')).toBeInTheDocument();
+})
+
+
+test('toggle', () => {
+    render(<TodoList />);
+    const task = screen.getByText('item 1');
+    fireEvent.click(task);
+  
+    expect(task).toHaveStyle('text-decoration: line-through');
+});
+
+
+test('delete', () => {
+    render(<TodoList />);
+    const deleteButton = screen.getAllByText('delete')[0];
+    fireEvent.click(deleteButton);
+  
+    expect(screen.queryByText('item 1')).not.toBeInTheDocument();
+});
