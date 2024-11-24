051ad1ab7539cb8a6896f08359c2f3a1039f3f9c
Omar Albaz <omaralbaz635@gmail.com>
2024-11-24 07:26:11 +0200

    creat : react-query-demo

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostsComponent from "./components/PostsComponent";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <h1>React Query Example</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Example</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
