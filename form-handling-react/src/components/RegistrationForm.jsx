bf34f304adf99e92a345d5ca4fa8372367df38de
Omar Albaz <omaralbaz635@gmail.com>
2024-11-24 06:38:47 +0200

    creat : form-handling-react

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -16,18 +16,36 @@ function RegistrationForm() {
     const handelpassword = (e) => {
         setpassword(e.target.value)
     }
-    const handelform = (e) => {
-        e.preventDefault()
-        console.log(`hello ${username} ,your email is ${email} and your password is ${password}`)
+    const handleForm = (e) => {
+        e.preventDefault();
+    
+        const newErrors = {};
+    
+        if (!username) {
+          newErrors.username = "Username is required";
+        }
+        if (!email) {
+          newErrors.email = "Email is required";
+        }
+        if (!password) {
+          newErrors.password = "Password is required";
+        }
+    
+        setErrors(newErrors);
+    
+        if (Object.keys(newErrors).length === 0) {
+          console.log("Form submitted:", { username, email, password });
+        }
     }
+    
 
 
   return (
     <>
        <form onSubmit={handelform}>
-            <input name='username' type="text" required value={username} onChange={handelusername}  placeholder='username'/> <br />
-            <input name='email' type="email" required value={email} onChange={handelemail} placeholder='email'/> <br />
-            <input name='password' type="password" required value={password} onChange={handelpassword} placeholder='password'/> <br />
+            <input name='username' type="text"  value={username} onChange={handelusername}  placeholder='username'/> <br />
+            <input name='email' type="email"  value={email} onChange={handelemail} placeholder='email'/> <br />
+            <input name='password' type="password"  value={password} onChange={handelpassword} placeholder='password'/> <br />
             <button type="submit">submit</button>
        </form>
     </>
