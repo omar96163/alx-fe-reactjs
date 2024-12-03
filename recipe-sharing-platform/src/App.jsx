be7de186f65069d57e9785b38830cb95b6e38ebe
Omar Albaz <omaralbaz635@gmail.com>
2024-12-03 19:22:01 +0200

    creat : recipe-sharing-platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HomePage from "./components/HomePage";
-import AddRecipeForm from "./components/AddRecipeForm ";
+import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetail from "./components/RecipeDetail";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
