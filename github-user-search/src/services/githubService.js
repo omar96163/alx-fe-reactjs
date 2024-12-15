5544629724a0f5cd7eab224043438f59178aee96
Omar Albaz <omaralbaz635@gmail.com>
2024-12-15 19:20:20 +0200

    creat :  github-user-search/src/components/Search.jsx

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://api.github.com/search/users";
 
-const fetchUserData = async (username, location, minRepos) => {
+const fetchUserData = async (username, location, minRepos, page = 1) => {
   try {
     let query = `in:login ${username}`;
 
@@ -13,7 +13,9 @@ const fetchUserData = async (username, location, minRepos) => {
       query += ` repos:>${minRepos}`;
     }
 
-    const response = await axios.get(`${BASE_URL}?q=${query}`);
+    const response = await axios.get(
+      `${BASE_URL}?q=${query}&page=${page}&per_page=30`
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching user data:", error);
