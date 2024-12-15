b4a5451e00e399c760c51fd7b665000974340ae1
Omar Albaz <omaralbaz635@gmail.com>
2024-12-15 19:22:01 +0200

    creat :  github-user-search/src/components/Search.jsx

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users";
+const BASE_URL = "https://api.github.com/search/users?q";
 
 const fetchUserData = async (username, location, minRepos, page = 1) => {
   try {
