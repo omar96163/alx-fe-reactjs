51956e361deb70d92b563c2473b138e5a13013f4
Omar Albaz <omaralbaz635@gmail.com>
2024-12-16 15:28:03 +0200

    creat : github-user-search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users";
+const BASE_URL = "https://api.github.com/search/users?q";
 
 /**
  * Fetch users from GitHub API with advanced search criteria
